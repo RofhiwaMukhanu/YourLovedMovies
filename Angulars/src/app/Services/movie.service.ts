45b7e20f6a9ddf880938829afb768d7d10beea31
DA M1 40 <dam140@DAs-MacBook-Air.local>
2025-03-14 09:14:22 +0200

    Angular project

--- Angulars/src/app/Services/movie.service.ts
+++ Angulars/src/app/Services/movie.service.ts
@@ -1,9 +1,37 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environments } from '../../environments/environments';
+
+// Define the Movie interface here (no extra file)
+export interface Movie {
+  id: number;
+  title: string;
+  poster_path: string;
+  release_date: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
+  private http = inject(HttpClient);
+  private apiKey = environments.tmdbApiKey; // Uses environment
+  private baseUrl = 'https://api.themoviedb.org/3';
 
-  constructor() { }
+  // Get popular movies
+
+
+  getPopularMovies() {
+    return this.http.get<{ results: Movie[] }>(
+    `  ${this.baseUrl}/movie/popular?api_key=${this.apiKey}`,
+      { headers: { 'Content-Type': 'application/json' } }
+    );
+  }
+
+  // Search movies
+  searchMovies(query: string) {
+    return this.http.get<{ results: Movie[] }>(
+     `${this.baseUrl}/search/movie?api_key=${this.apiKey}&query=${query}`
+    );
+  }
 }
